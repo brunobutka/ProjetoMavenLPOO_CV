1b07d80289f4a89812dc948fc6948914ae2ef50e
brunobutka <brunobutka2010@hotmail.com>
2022-01-04 16:40:30 -0300

    criação das pastas

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Agenda.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsul.cc.lpoo.cv.model;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author huriel
+ */
+@Entity
+@Table(name = "tb_agenda")
+public class Agenda implements Serializable {
+    
+    @Id
+    @SequenceGenerator(name = "seq_agenda", sequenceName = "seq_agenda_id", allocationSize = 1)
+    @GeneratedValue(generator = "seq_agenda", strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar data_inicio;
+    
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar data_fim;
+    
+    @Column(nullable = true)
+    private String observacao;
+    
+    public Agenda() {
+        
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the data_inicio
+     */
+    public Calendar getData_inicio() {
+        return data_inicio;
+    }
+
+    /**
+     * @param data_inicio the data_inicio to set
+     */
+    public void setData_inicio(Calendar data_inicio) {
+        this.data_inicio = data_inicio;
+    }
+
+    /**
+     * @return the data_fim
+     */
+    public Calendar getData_fim() {
+        return data_fim;
+    }
+
+    /**
+     * @param data_fim the data_fim to set
+     */
+    public void setData_fim(Calendar data_fim) {
+        this.data_fim = data_fim;
+    }
+
+    /**
+     * @return the observacao
+     */
+    public String getObservacao() {
+        return observacao;
+    }
+
+    /**
+     * @param observacao the observacao to set
+     */
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    
+ }
